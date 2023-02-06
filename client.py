bfb6761d1066dbac5fe1a3bfd00a32ba6b93f412
Evgeniy <zheshe@list.ru>
2023-02-06 11:53:57 +0300

    Homework

---
+++ client.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+import config
+
+
+def receiving():
+    global receiving_data
+    while True:
+        receiving_data += client.recv(1024).decode('ascii')
+        print(receiving_data)
+
+
+def write():
+    while True:
+        message = input()
+
+        if message == 'close':
+            client.close()
+
+        message = nickname + ': ' + message + '\n'
+        client.send(message.encode('ascii'))
+
+
+client = socket.socket()
+client.connect(config.socket)
+
+nickname = config.nickname
+client.send(nickname.encode('ascii'))
+
+receiving_data = ''
+
+receiving_thread = threading.Thread(target=receiving).start()
+write_thread = threading.Thread(target=write).start()
---
+++ config.py
@@ -0,0 +1,4 @@
+ip = '192.168.31.5'
+port = 55555
+socket = (ip, port)
+nickname = 'KvShe'
